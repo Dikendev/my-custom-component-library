aa9a94d4bf50779b6a4f2d862a9ca1354544ca46
Dikendev <diken.dev@gmail.com>
2024-02-09 22:10:59 -0300

    remove html angular boilerplate

--- src/app/app/components/button/button.component.ts
+++ src/app/app/components/button/button.component.ts
@@ -5,12 +5,20 @@ import {
   Input,
   ViewChild,
 } from '@angular/core';
+import { BaseIconButton } from './base-icon-button.component';
+import { ButtonSize } from './button.types';
 
 @Component({
   selector: 'dk-button',
   standalone: true,
   imports: [],
   styleUrl: './button.component.scss',
-  template: ``,
+  template: ` <button #button [id]="buttonId" [size]="size"></button> `,
 })
-export class ButtonComponent implements AfterViewInit {}
+export class ButtonComponent extends BaseIconButton implements AfterViewInit {
+  static iconButtonCounter = 0;
+
+  @ViewChild('button') button: ElementRef;
+
+  @Input() size: ButtonSize = 'lg';
+}
