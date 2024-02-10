b7a9f340ab6b8c05c1096cf42e081f65015c8492
Dikendev <diken.dev@gmail.com>
2024-02-09 22:10:38 -0300

    feat add button type

---
+++ src/app/app/components/button/button.types.ts
@@ -0,0 +1,3 @@
+export type DangerButtonType = 'danger--primary' | 'danger--tertiary';
+export type ButtonType = 'primary' | 'secondary' | DangerButtonType;
+export type ButtonSize = 'sm' | 'md' | 'lg' | 'xl' | '2xl';
